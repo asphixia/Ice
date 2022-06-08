0ad38abada41ee246d3c154e0b1bcc5ca8c325a8
asphixia <60938767+asphixia@users.noreply.github.com>
2022-06-08 22:17:50 +0200

    Update ScoreBoard.java

--- ScoreBoard.java
+++ ScoreBoard.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class ScoreBoard {
+    private int score = 0;
+    JLabel sc;
+
+    public ScoreBoard(){
+        displayScore();
+
+    }
+    public void displayScore(){
+        sc = new JLabel("Score: " + score);
+        sc.setBounds(0, 0, 600, 50);
+        sc.setFont(new Font(null, Font.PLAIN,25));
+        sc.setForeground(Color.WHITE);
+
+    }
+
 }
